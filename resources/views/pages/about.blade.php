fd903839a0a443dd2d06c8d5eaa52f36f814b409
isopope <nkoubah@gmail.com>
2022-05-10 20:55:14 +0000

    video 4

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello city</title>
-
-    </head>
-    <body>
+@section('content')
         <p>Built with Laravel 8 &hearts; by Yves and Humbert</p>
         <p><a href="/">Revenir a la page d'acceuil</a></p>
-        <footer>
-            <p>&copy;Copyright {{date('Y')}}</p>
-        </footer>
-       
-    
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello city</title>
-
-    </head>
-    <body>
+@section('content')
         <h1>Hello from Togo</h1>
         <p>Il est actuellement {{date('h:i A')}}</p>
-        <footer>
-            <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About-us</a></p>
-        </footer>
-       
-    
-    </body>
-</html>
+@endsection
