cda9744ae7a3bd1601a19d92a8b1b84a104d80ed
isopope <nkoubah@gmail.com>
2022-05-10 23:12:44 +0000

    stylisation

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,9 @@
 @section('title','About-us | '.config('app.name'))
 
 @section('content')
-         <img src="{{asset('/images/télécharger.png')}}" alt="logo de l'ucao"/>
-        <p>Built with Laravel 8 &hearts; by Yves and Humbert</p>
-        <p><a href="{{route('app_home')}}">Revenir a la page d'acceuil</a></p>
+         <img src="{{asset('/images/télécharger.png')}}" alt="logo de l'ucao" class=" my-12 rounded-full"/>
+        <h2 class=" mb-5 text-gray-700">
+                Built with Laravel 8 <span class="text-pink-500">&hearts;</span> by Yves and Humbert
+        </h2>
+        <p><a href="{{route('app_home')}}"class="text-indigo-500" hover:text-indigo-600 underline>Revenir a la page d'acceuil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 @section('title',' Hello city')
 
 @section('content')
-<img src="{{asset('/images/flag_of_togo.svg')}}" alt="drapeau togolais"/>
-        <h1>Hello from Togo</h1>
-        <p>Il est actuellement {{date('h:i A')}}</p>
+<img src="{{asset('/images/flag_of_togo.svg')}}" alt="drapeau togolais" class="mt-12 rounded shadow-md h-32" />
+        <h1 class="mt-5  text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Togo</h1>
+        <p class="text-lg text-gray-800">Il est actuellement {{date('h:i A')}}</p>
 @endsection
