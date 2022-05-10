46ddbddc1c8cc9bbd95264562d69d87491cdf035
isopope <nkoubah@gmail.com>
2022-05-10 22:20:58 +0000

    images 10

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,7 @@
 @section('title',' Hello city')
 
 @section('content')
+<img src="/images/flag_of_togo.svg" alt="drapeau togolais"/>
         <h1>Hello from Togo</h1>
         <p>Il est actuellement {{date('h:i A')}}</p>
 @endsection
